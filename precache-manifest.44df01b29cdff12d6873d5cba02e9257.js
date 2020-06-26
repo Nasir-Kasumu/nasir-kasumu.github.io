f38d9a230b88ab49f67ff37bd23babc6158849c8
Nasir-Kasumu <naskas1234@gmail.com>
2020-06-26 10:25:16 -0400

    Updates

---
+++ precache-manifest.44df01b29cdff12d6873d5cba02e9257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8b1eeb6205d5c609fd0dae1fc21d23",
+    "url": "/Nasir-Kasumu.github.io/index.html"
+  },
+  {
+    "revision": "f58961c614b9483e9c0e",
+    "url": "/Nasir-Kasumu.github.io/static/css/main.105ad762.chunk.css"
+  },
+  {
+    "revision": "095644e2a238e3c68695",
+    "url": "/Nasir-Kasumu.github.io/static/js/2.9586cf09.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/Nasir-Kasumu.github.io/static/js/2.9586cf09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f58961c614b9483e9c0e",
+    "url": "/Nasir-Kasumu.github.io/static/js/main.0eddaf66.chunk.js"
+  },
+  {
+    "revision": "115fcd312b68a79b47c3",
+    "url": "/Nasir-Kasumu.github.io/static/js/runtime-main.27312c69.js"
+  }
+]);
