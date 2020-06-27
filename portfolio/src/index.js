44588794b01f725684ef2fc512933ce9c8859797
Nasir-Kasumu <naskas1234@gmail.com>
2020-06-27 11:00:29 -0400

    adjusted json

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -4,9 +4,15 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+{/*import { BrowserRouter as Router} from 'react-router-dom';*/}
+
 ReactDOM.render(
   <React.StrictMode>
+    {/*</React.StrictMode><Router basename={process.env.PUBLIC_URL}>}
+    
     <App />
+    
+    {/*</Router>*/}
   </React.StrictMode>,
   document.getElementById('root')
 );
